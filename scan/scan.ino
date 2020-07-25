7ed710dbed9da8c9671ef2d6d3b26ade6d3f9cb3
sday7 <smnuernberger@gmail.com>
2020-07-25 15:38:51 -0400

    Added EEPROM .read() & .write() to pull out WiFi info to reconnect to previously connected WiFi.

--- scan/scan.ino
+++ scan/scan.ino
@@ -4,26 +4,62 @@
 #include <EEPROM.h>
 
 String clientName = "";
+String ssid = "";
+String password = "";
+String Essid = "";
+String Epassword = "";
+String EclientName = "";
+byte state = 0;
 
 ESP8266WebServer server(80); //Server on port 80
 WiFiServer wifiServer(80);
 WiFiClient client = wifiServer.available();
 
-void setUpAccessPoint() {
-    Serial.println('\n');
-    WiFi.mode(WIFI_AP);  
-    WiFi.softAP(clientName);
-    IPAddress myIP = WiFi.softAPIP(); //Get IP address
-    Serial.print("HotSpot IP:");
-    Serial.println(myIP);
-   
-    server.begin();                  //Start server
-    Serial.println("HTTP server started");
-  
-    server.on("/", handleRoot);      //Which routine to handle at root location
-    server.on("/scan", HTTP_POST, scanNetworks);
-    //server.on("/settings", HTTP_PUT, handleSettings);
 
+void setUpAccessPoint() {
+  Serial.println('\n');
+  WiFi.mode(WIFI_AP);  
+  WiFi.softAP(clientName);
+  IPAddress myIP = WiFi.softAPIP(); //Get IP address
+  Serial.print("HotSpot IP:");
+  Serial.println(myIP);
+ 
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+
+  state = 0;
+
+  server.on("/", handleRoot);      //Which routine to handle at root location
+  server.on("/scan", HTTP_POST, scanNetworks);
+  server.on("/settings", HTTP_PUT, handleSettings);
+}
+
+void accessWifi() {
+  int connectionTimeout = 0;
+  Serial.print("Connecting to  " + ssid); //Print out connecting to the WIFI network
+  WiFi.begin(ssid, password); //Credentials
+
+  while (WiFi.status() != WL_CONNECTED && connectionTimeout < 7 ) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(0, HIGH);
+    delay(500);
+    digitalWrite(0, LOW);
+    delay(500);
+    connectionTimeout++;
+  }
+  if(WiFi.status() != WL_CONNECTED || ssid == "" || ssid == NULL) {
+    delay(10000); //TODO: Delay might need to be extended at later time.
+    return;
+  } else {
+    WiFi.softAPdisconnect (true);
+    Serial.println(" connected"); 
+    state = 1; 
+    Serial.printf(WiFi.localIP().toString().c_str());
+    delay(5000);
+    EEPROM.commit();
+    EEPROM.end();
+  }
 }
 
 String htmlPage =
@@ -46,31 +82,92 @@ void handleRoot() {
 }
 
 void scanNetworks() {
-    delay(10000);
+  delay(10000);
 
-    int networks = WiFi.scanNetworks();
-    String str = "";
-    String fullString = "";
-    
-    str = "<ul>";
-    for(int i = 0; i < networks; i++) {
-        //Serial.println(WiFi.SSID(i));
-        str += "<li>";
-        str += i + 1;
-        str += ": ";
-        str += WiFi.SSID(i);
-        str += "</li>";
+  int networks = WiFi.scanNetworks();
+  String str = "";
+  String fullString = "";
+  
+  str = "<ul>";
+  for(int i = 0; i < networks; i++) {
+    str += "<li>";
+    str += i + 1;
+    str += ": ";
+    str += WiFi.SSID(i);
+    str += "</li>";
+  }
+  str += "</ul>";
+
+  fullString = "\n\r\n<!DOCTYPE HTML>\r\n<html><h1>WiFi List</h1> ";
+  fullString += "<p>";
+  fullString += str;
+  fullString += "</thml>\r\n\r\n";
+
+  server.send(200, "text/html", fullString);
+  Serial.println();
+  delay(10000);
+}
+
+int addressLocation = 0;
+void writeValue(String writeString) {
+  int stringLength = writeString.length();
+  for(int i = 0; i < stringLength; i++) {
+    EEPROM.write(addressLocation + i, writeString[i]);
+  } 
+  addressLocation += stringLength;
+  EEPROM.write(addressLocation, 0);
+  addressLocation += 1; 
+}
+
+int counter = 0;
+void readValue() {
+  delay(10);
+  char tempArray[][64] = {"","",""};
+  int number = 0;
+  for(int i = 0; i < 3; i++) {
+   for(int j = 0; j < 64; j++) {
+      char checkValue = EEPROM.read(number);
+      if(checkValue == 0) {
+        number++;
+        break;
+      }
+      tempArray[i][j] = checkValue;
+      number ++;
     }
-    str += "</ul>";
+    Serial.println(tempArray[i]);
+  }
+  clientName = String(tempArray[0]);
+  ssid = String(tempArray[1]);
+  password = String(tempArray[2]);
+}
 
-    fullString = "\n\r\n<!DOCTYPE HTML>\r\n<html><h1>WiFi List</h1> ";
-    fullString += "<p>";
-    fullString += str;
-    fullString += "</thml>\r\n\r\n";
+void sendToMemory() {
+  writeValue(clientName);
+  writeValue(ssid);
+  writeValue(password);
+  //EEPROM.commit();
+  //EEPROM.end();
+}
 
-    server.send(200, "text/html", fullString);
-    Serial.println();
-    delay(10000);
+void handleSettings() {
+  if(server.hasArg("ssid") && server.hasArg("password")) {
+    ssid = server.arg("ssid");
+    password = server.arg("password");
+    if(server.hasArg("nickname")) {
+      clientName = server.arg("nickname");
+    }
+    sendToMemory();
+    server.send(202);
+    accessWifi();
+  } else {
+      if(server.hasArg("ssid") || server.hasArg("password")) {
+        handleBadRequest();   
+      }
+  }
+  if(server.hasArg("nickname") && !server.hasArg("ssid") && !server.hasArg("password")) {
+    clientName = server.arg("nickname");
+    sendToMemory();
+  }
 }
 
 void handleNotAccepted() {
@@ -82,18 +179,29 @@ void handleBadRequest() {
 }
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println();
+  Serial.begin(115200);
+  EEPROM.begin(256);
+  Serial.println();
 
-    if(clientName == "") {
-      clientName = "ESP8266 AP";
-    }
+  readValue();
 
-    WiFi.mode(WIFI_STA);
-    delay(1000);
-    setUpAccessPoint();
+  if(clientName == "") {
+    clientName = "ESP8266 AP";
+  }
+
+  WiFi.mode(WIFI_STA);
+  delay(1000);
+  setUpAccessPoint();
+  accessWifi();
 }
 
 void loop() {
-    server.handleClient();
+  server.handleClient();
+  if(state == 0 && (ssid != "" || ssid != NULL) && (password != "" || password != NULL)) {
+    accessWifi();
+    Serial.println(" LINE 200  ");
+  } else if(WiFi.status() != WL_CONNECTED) {
+    Serial.println( " LINE 204  ");
+    setUpAccessPoint();
+  }
 }
